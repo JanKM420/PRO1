d667d4cc5d36ebb9b205d4d7dfc5aadc17fafb95
Jan Kodrič <j.kodric11@gmail.com>
2023-02-13 10:01:10 +0100

    VajaPro2

---
+++ VajaPRO2/VajaPRO2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaPRO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vsota = 0;
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int st = random.Next(0, 100);
+                vsota += st;
+                Console.WriteLine("Naključna številka {0}: {1}", i + 1, st);
+            }
+            Console.WriteLine("Vsota teh 100 števil je: {0}", vsota);
+            Console.ReadLine();
+        }
+    }
+}
