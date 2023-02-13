d2f0fb8d2e42abe657e460b916726e84f901917c
Jan Kodrič <j.kodric11@gmail.com>
2023-02-13 10:53:58 +0100

    vaja45

---
+++ vA/vA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vsota = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                vsota += Math.Sin(Math.PI / i);
+            }
+            Console.WriteLine("Vsota je: {0}", vsota);
+            Console.ReadLine();
+        }
+    }
+}
