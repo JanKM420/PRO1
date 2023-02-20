c33862ab6f08edf13683468ae05083b2ecf70cd2
Jan Kodrič <j.kodric11@gmail.com>
2023-02-20 10:35:29 +0100

    vaja

---
+++ VajaPravokotnik/VajaPravokotnik/Pravokotnik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaPravokotnik
+{
+    internal class Pravokotnik
+    {
+        private readonly int dolzina;
+        private readonly int sirina;
+
+        public Pravokotnik(int dolzina, int sirina)
+        {
+            this.dolzina = dolzina;
+            this.sirina = sirina;
+        }
+        public int GetArea()
+        {
+            return dolzina * sirina;
+        }
+        public int GetPerimeter()
+        {
+            return 2 * (dolzina + sirina);
+        }
+        public bool IsSquare()
+        {
+            return dolzina == sirina;
+        }
+    }
+}
+
---
+++ VajaPravokotnik/VajaPravokotnik/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaPravokotnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pravokotnik a = new Pravokotnik(5, 8);
+            Console.WriteLine($"Pravokotnik a: površina = {a.GetArea()}, obseg = {a.GetPerimeter()}, kvadrat = {a.IsSquare()}");
+
+            Pravokotnik b = new Pravokotnik(6, 6);
+            Console.WriteLine($"Pravokotnik b: površina = {b.GetArea()}, obseg = {b.GetPerimeter()}, kvadrat = {b.IsSquare()}");
+        }
+    }
+}
+    
+
